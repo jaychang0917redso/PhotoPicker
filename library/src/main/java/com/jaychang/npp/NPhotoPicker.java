e8e5fe2eae2d1f64aefa1f87230dabbecc6dcbcd
Jay <jaychang0917@gmail.com>
2016-09-29 12:14:54 +0800

    Update NPhotoPicker.java

--- library/src/main/java/com/jaychang/npp/NPhotoPicker.java
+++ library/src/main/java/com/jaychang/npp/NPhotoPicker.java
@@ -1,7 +1,7 @@
 package com.jaychang.npp;
 
 import android.app.Activity;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Parcelable;
 import android.support.annotation.ColorRes;
